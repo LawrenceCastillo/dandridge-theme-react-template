e55d3829f5c31796311a256f2ea76c526ce35d75
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-11-18 21:39:20 -0500

    Add content sheet with api structure, install helmet dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import './App.css'
+import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import { Homepage } from './pages/homepage'
 
-export const App = () => (
-  <div>
-    <Homepage />
-  </div>
-)
+import { user } from './content.js'
+
+import './App.css'
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      personal: {},
+    }
+  }
+
+  componentDidMount() {
+    fetch('') // API endpoint 
+    .then(response => response.json())
+    .then(userAPI => this.setState({ 
+      personal: userAPI.personal[0],
+    }))
+    .catch(err => {
+      console.log(err)
+      this.setState({
+        personal: user.personal[0],
+      })
+    })
+  }
+
+  render() {
+    const { personal } = this.state
+    return (
+      <div>
+        { personal.name ? 
+        <div>
+          <Helmet>
+            <title> {personal.name} | {personal.headline} </title>
+            <link 
+              rel="canonical" 
+              href={personal.domain} />
+            <meta 
+              name="description" 
+              content={personal.short_bio} />
+            <meta 
+              name="keywords" 
+              content={personal.keywords} />
+          </Helmet>
+
+          <Homepage />
+        </div>
+
+        : <div className='loading'> Loading... </div>
+      }
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/content.js
@@ -0,0 +1,39 @@
+/* Sitewide content */
+
+export const user = {
+    personal: [
+      {
+        section: 'Personal',
+        name: 'Xiomara Tejera',
+        email: '',
+        domain: '',
+        keywords: 'xiomara tejera, actor, actress, new york city, new york, film, tv, commercial, movie',
+        background_url: '/images/xiomara-tejera-actor-actress-new-york-city.jpg',
+        location: 'NYC',
+        headline: '',
+        short_bio: '',
+        image: '',
+        long_bio: '',
+        skills:
+        [
+          ''
+        ],
+        social:       
+        [
+          {
+            id: 1,
+            name: 'Instagram',
+            icon_url: '',
+            profile_url: ''
+          },
+        ],
+        interests:
+        [
+          '',
+        ],
+      },
+    ],
+    groups: [
+      
+    ],
+  }
