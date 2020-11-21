58342f335d3b5b5e754c5fd39a17150be63b4137
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-11-20 20:41:51 -0500

    Add and style data to full-bg layout

--- src/layouts/full_background_image/full_bg_img.jsx
+++ src/layouts/full_background_image/full_bg_img.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import './full_bg_img.scss'
 
-export const LayoutFullBackgroundImage = ({ bg_img }) => (
+export const LayoutFullBackgroundImage = ({ bg_img, name, fname, lname, headline }) => (
   <div className='header'>
     <div 
       className='header-bg'
-      style={{ backgroundImage: `url(${bg_img})`}}>
+      style={{ backgroundImage: `url(${bg_img})`}}
+    >
+      <h1 className='header-title'>{name}</h1>
+      <h2 className='header-subtitle'>{headline}</h2>
     </div>
   </div>
 )
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,9 +6,19 @@ import './homepage.scss'
 
 export const Homepage = props => {
   const bg_img = props.personal.background_url
+  const name = props.personal.name
+  const fname = props.personal.fname
+  const lname = props.personal.lname
+  const headline = props.personal.headline
   return (
     <div className='header'>
-      <LayoutFullBackgroundImage bg_img={bg_img} />
+      <LayoutFullBackgroundImage 
+        bg_img={bg_img} 
+        name={name}
+        fname={fname}
+        lname={lname}
+        headline={headline} 
+      />
     </div>
   )
 }
