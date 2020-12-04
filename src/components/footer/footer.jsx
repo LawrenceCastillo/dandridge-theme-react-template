00c1bd5d7961b51e8405d4ae21affaebf8c4e1e6
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-12-04 13:43:52 -0500

    Add content to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,35 @@ import React from 'react'
 import './footer.scss'
 
 export const Footer = () => (
-  <div className='footer'>
+  <div 
+    className='footer'
+    style={{ backgroundImage: 'URL(/images/bg-social.jpg)'}}
+  >
     <div className='footer-container'>
-      Footer
+      <a 
+        className='footer-icon'
+        href='http://example.com/' 
+        target="_blank" 
+        rel='noopener noreferrer'
+      >
+        <img 
+          aria-label='visit instagram' 
+          src='/images/social/instagram-logo.png' 
+          height='140px' 
+        />
+      </a>
+      <a 
+        className='footer-icon'
+        href='http://example.com/' 
+        target="_blank" 
+        rel='noopener noreferrer'
+      >
+        <img 
+          aria-label='media' 
+          src='/images/social/backstage-logo.png'
+          height='140px' 
+        />
+      </a>
     </div>
   </div>
 )
